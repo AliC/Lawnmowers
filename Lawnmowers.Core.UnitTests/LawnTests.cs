fcb522882203aac0a9af00da6ecacd7f9ce96752
Alastair Danley-Cox <alastair_cox@btinternet.com>
2016-09-06 07:21:36 +0100

    Added test for creation of lawn

---
+++ Lawnmowers.Core.UnitTests/LawnTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lawnmowers.Core.UnitTests
+{
+    [TestFixture]
+    public class LawnTests
+    {
+        [Test]
+        public void When_Coordinates_Are_Five_By_Five_Then_Lawn_Is_Created_Five_By_Five()
+        {
+            int expectedX = 5;
+            int expectedY = 5;
+
+            Lawn actualLawn = Lawn.Create(expectedX, expectedY);
+
+            Assert.IsNotNull(actualLawn);
+        }
+    }
+}
---
+++ Lawnmowers.Core/Lawn.cs
@@ -0,0 +1,20 @@
+﻿namespace Lawnmowers.Core
+{
+    public class Lawn
+    {
+        public int X { get; }
+
+        public int Y { get; }
+
+        public Lawn(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Lawn Create(int x, int y)
+        {
+            return new Lawn(x, y);
+        }
+    }
+}
