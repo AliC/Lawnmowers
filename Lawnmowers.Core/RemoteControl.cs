824a7028fbf4bec540487c86d09857028cf89d9a
Alastair Danley-Cox <alastair_cox@btinternet.com>
2016-09-07 08:12:24 +0100

    Added missing files from last check-in

---
+++ Lawnmowers.Core/RemoteControl.cs
@@ -0,0 +1,17 @@
+﻿namespace Lawnmowers.Core
+{
+    public class RemoteControl
+    {
+        private Mower _mower;
+
+        public void ConnectMower(Mower mower)
+        {
+            _mower = mower;
+        }
+
+        public void Send(string instructions)
+        {
+            _mower.Navigate(instructions);
+        }
+    }
+}
